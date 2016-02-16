b38fe947b4bde376fdc8b9a47cf05c2cf887aa8a
Seva Zhidkov <zhidkovseva@gmail.com>
2016-02-16 13:57:24 +0400

    Add more parameters for elements

--- scripts/parse_en_elements.py
+++ scripts/parse_en_elements.py
@@ -11,11 +11,23 @@ root = lxml.html.fromstring(body)
 elements = []
 
 for i in range(3, TABLE_ELEMENTS + 3):
-    row = list(root.xpath('//*[@id="mw-content-text"]/table[1]/tr')[i].iterfind('td'))
+    row = root.xpath('//*[@id="mw-content-text"]/table[1]/tr')[i]
+    columns = list(row.iterfind('td'))
+    links = list(row.iterlinks())
+    if columns[6].findall('span'):
+        atomic_weight = str(columns[6].xpath('span[2]')[0].text_content())
+    else:
+        atomic_weight = str(columns[6].text_content())
+    element_link = 'https://en.wikipedia.org{}'.format(links[0][2])
     elements.append({
-        'num': int(row[0].text_content()),
-        'sym': str(row[1].text_content()),
-        'name': str(row[2].text_content())
+        'num': int(columns[0].text_content()),
+        'sym': str(columns[1].text_content()),
+        'name': str(columns[2].text_content()),
+        'group': str(columns[4].text_content()),
+        'period': str(columns[5].text_content()),
+        'atomic_weight': atomic_weight,
+        'density': str(columns[7].text_content()) + ' g/cm^3',
+        'source': element_link
     })
 
 with open('data/en/elements.yml', 'w') as elements_file:
